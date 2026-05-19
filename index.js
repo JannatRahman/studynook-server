6b01c56f84af9a7ac4e82a26f2aa05bda414b3bd
JannatRahman <you@example.com>
2026-05-19 15:44:29 +0600

    mongodb connected with backend server

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+dotenv.config();
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+const uri = "mongodb://studynook:6w1QuiTPu7iSMO87@ac-q3n4nqa-shard-00-00.erd7kb0.mongodb.net:27017,ac-q3n4nqa-shard-00-01.erd7kb0.mongodb.net:27017,ac-q3n4nqa-shard-00-02.erd7kb0.mongodb.net:27017/?ssl=true&replicaSet=atlas-s39w7z-shard-0&authSource=admin&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    const db = client.db('studynook');
+    const studyroomsCollection = db.collection('studyrooms');
+
+    app.get('/studyrooms'), async (req, res) => {
+      const cursor = studyroomsCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+    }
+    
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
