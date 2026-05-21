37509899dbc2abc017d8ed670e2288c86a86c34c
JannatRahman <you@example.com>
2026-05-21 21:56:08 +0600

    post method used for adding room

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
-const uri =process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI
 
- const JWKS = createRemoteJWKSet(
-      new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
-      console.log(JWKS,
-        'FROM JWKS'
-      );
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+console.log(JWKS,
+  'FROM JWKS'
+);
 
 
 const client = new MongoClient(uri, {
@@ -27,58 +27,65 @@ const client = new MongoClient(uri, {
   }
 });
 
-const logger = (req, res, next)    => {
-      console.log(`${req.method} | ${req.url}`);
-      next();
-    } ;
-  
-    const verifyToken = async (req, res, next) => {
-      const {authorization} = req.headers;
-      const token = authorization?.split(' ')[1];
+const logger = (req, res, next) => {
+  console.log(`${req.method} | ${req.url}`);
+  next();
+};
 
- if(!token){
-  return res.status(401).json({message: 'Unauthorized'});
- };
-    try {
+const verifyToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const token = authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
+  try {
     const JWKS = createRemoteJWKSet(
       new URL('http://localhost:3000/api/auth/jwks')
     )
 
     const { payload } = await jwtVerify(token, JWKS,);
     req.user = payload;
-//  console.log(req.user);
+    //  console.log(req.user);
 
     next();
   } catch (error) {
     console.error('Token validation failed:', error);
 
-    return res.status(401).json({message: 'Unauthorized'});
+    return res.status(401).json({ message: 'Unauthorized' });
   }
-  };
+};
 
- async function run() {
+async function run() {
   try {
-    
+
     await client.connect();
 
-// 1. CONNECTED WITH MONGODB
+    // 1. CONNECTED WITH MONGODB
     const db = client.db('studynook');
     const studyroomsCollection = db.collection('studyrooms');
     const bookingCollection = db.collection('booking');
 
+     app.post('/studyrooms', async (req, res) => {
+      const roomData = req.body
+      console.log(roomData);
+      
+      const result = await studyroomsCollection.insertOne(roomData)
 
+      res.json(result);
+     })
 
     app.get('/studyrooms', async (req, res) => {
-      const {search} = req.query;
+      const { search } = req.query;
 
       let cursor;
-      if(search) {
-        cursor = studyroomsCollection.find({name: {$regex: search, $options: 'i'}});
+      if (search) {
+        cursor = studyroomsCollection.find({ name: { $regex: search, $options: 'i' } });
 
-        console.log( 'from search')
-        
-        
-      }else{
+        console.log('from search')
+
+
+      } else {
         cursor = studyroomsCollection.find();
       };
 
@@ -98,53 +105,56 @@ const logger = (req, res, next)    => {
 
 
     app.get('/studyrooms/:studyroomsId', logger, verifyToken,
-   async (req, res) => {
-    // console.log(req.user, 'req');
-    
-     const {studyroomsId} = req.params;
-    const query = {_id: new ObjectId(studyroomsId)};
-    const result = await studyroomsCollection.findOne(query);
-    res.send(result);
-    });
+      async (req, res) => {
+        // console.log(req.user, 'req');
 
-   app.get('booking/:userId', async (req, res) => {
-    const {userId} = req.params;
-    const result = await bookingCollection.find({userId: userId}).toArray();
-    res.send(result);
-   })
+        const { studyroomsId } = req.params;
+        const query = { _id: new ObjectId(studyroomsId) };
+        const result = await studyroomsCollection.findOne(query);
+        res.send(result);
+      });
+
+    app.get('/booking/:userId', verifyToken, async (req, res) => {
+      const { userId } = req.params;
+      const result = await bookingCollection.find({ userId: userId }).toArray();
+      res.send(result);
+    })
 
 
-    app.patch('/booking/:studyroomsId',verifyToken,  async (req, res) => {
-      const {studyroomsId} = req.params;
+    app.patch('/booking/:studyroomsId', verifyToken, async (req, res) => {
+      const { studyroomsId } = req.params;
       const bookingData = req.body;
 
-      const studyrooms = await studyroomsCollection.findOne({_id: new ObjectId(studyroomsId)});
+      const studyrooms = await studyroomsCollection.findOne({ _id: new ObjectId(studyroomsId) });
 
-      if(!studyrooms) {
-        res.status(404).json({message: "Rooms not found"});
+      if (!studyrooms) {
+        return res.status(404).json({
+          message: "Rooms not found"
+        });
       }
-      await studyroomsCollection.updateOne({_id: new ObjectId(studyroomsId)}, 
-    {
-      $inc: {bookingCount: 1},
-      $set: {
-        lastBookingAt: new Date(),
-      },
-    }
-  );
-   const result = await bookingCollection.insertOne({
-    ...bookingData,
-    bookedAt: new Date(),
-   });
-   res.send(result);
+      await studyroomsCollection.updateOne({ _id: new ObjectId(studyroomsId) },
+        {
+          $inc: { bookingCount: 1 },
+          $set: {
+            lastBookingAt: new Date(),
+          },
+        }
+      );
+      const result = await bookingCollection.insertOne({
+        ...bookingData,
+        userId: req.user.id,
+        bookedAt: new Date(),
+      });
+      res.send(result);
     });
 
 
 
-    
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-   
+
     // await client.close();
   }
 }
